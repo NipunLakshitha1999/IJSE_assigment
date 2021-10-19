79288c28200cec39f1a8a1fcef089625b60a9a92
Nipun Lakshitha <nipunlakshithasilva1999@gmail.com>
2021-10-19 22:38:46 +0530

    item get all ,search created , not workd 100%

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 
-__item_last_id__=f"db/item/item_last_id.db"
+__item_last_id__=f"db/itemIdDerectory/item_last_id.db"
+__item_folder__="db/item"
 
 import json
 import os
+from pprint import pprint
 
 
 class Item:
@@ -11,7 +13,7 @@ class Item:
 
     def save(self):
         if os.path.exists(__item_last_id__):
-            with open(f"db/item/item_last_id.db", "r") as itemLastId:
+            with open(f"db/itemIdDerectory/item_last_id.db", "r") as itemLastId:
                 last_id_item = json.load(itemLastId)
                 self.last_id = last_id_item["id"] + 1
 
@@ -35,8 +37,35 @@ class Item:
         }
 
 
-        with open(f"db/item/item_last_id.db","w") as itemLastId:
+        with open(f"db/itemIdDerectory/item_last_id.db","w") as itemLastId:
             json.dump(_last_id,itemLastId)
+
+
+    def all(self):
+        item_file_names= os.listdir(__item_folder__)
+        items=[]
+        for item_file_name in item_file_names:
+            item =Item()
+            Item.__get_item_by_path(
+                item,f"{__item_folder__}/{item_file_name}")
+            items.append(item)
+            print(items)
+        return items
+
+
+    def __get_item_by_path(item, path):
+        with open(path, "r") as item_file:
+            _data_ = json.load(item_file)
+
+    def search(self, key, value):
+        items = self.all()
+        result_items = []
+        for item in items:
+            item_value = getattr(item, key)
+            print(item_value)
+            if item_value == value:
+                result_items.append(item)
+        return result_items
 
 def item_create(name, price, selling_price):
     item = Item()
@@ -45,15 +74,35 @@ def item_create(name, price, selling_price):
     item.selling_price = selling_price
     item.save()
 
+def item_all():
+    item=Item()
+    items=item.all()
+    pprint(items.__str__())
+
+def item_search(key,value):
+    item = Item()
+    results = item.search(key,value)
+    pprint(results)
+
 if __name__ == "__main__":
     section_name=input("Enter Section Name(Item/Order/Customer)")
 
     if section_name == "Item":
-        print("Plz Enter Item Details:-")
-        item_name=input("Item Name")
-        item_price=input("Item Price")
-        item_selling_price=input("Item Selling Price")
-    item_create(item_name,item_price,item_selling_price)
+        sub_section_item=input("Plz enter View/Save/Serach")
+        if sub_section_item == "Save":
+            print("Plz Enter Item Details:-")
+            item_name=input("Item Name")
+            item_price=input("Item Price")
+            item_selling_price=input("Item Selling Price")
+            item_create(item_name,item_price,item_selling_price)
+
+        elif sub_section_item == "View":
+            item_all()
+
+        elif sub_section_item== "Search":
+            key=input("Key")
+            value=input("Value")
+            item_search(key,value)
 
 
     if section_name == "Order":
